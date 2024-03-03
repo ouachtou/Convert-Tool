b6677d9c182ae2ab792caaab059060c5265419ca
ouachtou <pierre.legrand1105@gmail.com>
2024-03-03 18:02:15 +0100

    python

--- ConvertTool/GUI/GUI.py
+++ ConvertTool/GUI/GUI.py
@@ -1,10 +1,29 @@
 # coding: utf-8
-
+import string
 from tkinter import *
 
 fenetre = Tk()
 
 label = Label(fenetre, text="Hello World")
 label.pack()
+
+# entrée
+value = StringVar()
+value.set("texte par défaut")
+entree = Entry(fenetre, textvariable=string, width=30)
+entree.pack()
+
+liste = Listbox(fenetre)
+liste.insert(1, "Python")
+liste.insert(2, "PHP")
+liste.insert(3, "jQuery")
+liste.insert(4, "CSS")
+liste.insert(5, "Javascript")
+
+liste.pack()
+
+# bouton de sortie
+bouton=Button(fenetre, text="Fermer", command=fenetre.quit)
+bouton.pack()
 
 fenetre.mainloop()
