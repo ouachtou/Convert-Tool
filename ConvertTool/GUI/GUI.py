702a8bbf802bc9d68e24adadf03ff4114d9f7942
ouachtou <pierre.legrand1105@gmail.com>
2024-03-03 18:53:53 +0100

    converting

--- ConvertTool/GUI/GUI.py
+++ ConvertTool/GUI/GUI.py
@@ -5,34 +5,49 @@ from tkinter import ttk
 
 Window = Tk()
 Window.title('ConverTool')
-Window.geometry('300x200')
-Window.config(background="white")
+Window.geometry('500x300')
 
 
-#------------------Choix_entrée------------------#
-labelChoix = tk.Label(Window, text="Choix d'entrée:")
-labelChoix.pack()
+#------------------Choix_entré------------------#
+labelChoices = tk.Label(Window, text="Choix d'entrée:")
+labelChoices.pack()
 
 listeProduits = ["binary", "decimal", "hexadecimal"]
-listeCombo = ttk.Combobox(Window, values=listeProduits)
-listeCombo.current(0)
-listeCombo.pack()
+listEntry = ttk.Combobox(Window, values=listeProduits)
+listEntry.current(0)
+listEntry.pack()
 
 #------------------Choix_sortie------------------#
-labelChoix = tk.Label(Window, text="Choix de sortie")
-labelChoix.pack()
+labelChoices = tk.Label(Window, text="Choix de sortie")
+labelChoices.pack()
 
 listeProduits = ["binary", "decimal", "hexadecimal"]
-listeCombo = ttk.Combobox(Window, values=listeProduits)
-listeCombo.current(1)
-listeCombo.pack()
+listOutput = ttk.Combobox(Window, values=listeProduits)
+listOutput.current(1)
+listOutput.pack()
 
 #------------------ToConvert------------------#
-dec=StringVar()
+txt = StringVar()
+txt.set("Number to convert")
+txtEntry = Entry(Window, textvariable= txt)
+txtEntry.pack()
+
+#------------------ConvertButton------------------#
+Button(text='-->').pack()
+
+#------------------Converted------------------#
+txt = StringVar()
+txt.set("Output")
+txtOutput = Entry(Window, textvariable= txt)
+txtOutput.pack()
+Window.mainloop()
+
+
+#------------------ToConvert------------------#
+"""dec=StringVar()
 zone1 = Entry(Window, textvariable= dec, bg ='purple', fg='white')
 zone1.grid(row=3, column=2,pady=5)
-dec.set('décimal')
-Window.mainloop()
+dec.set('décimal')"""
 
 #------------------Converting------------------#
 """Button_convert = Button(Window, text = 'Convert',command=calcul())
